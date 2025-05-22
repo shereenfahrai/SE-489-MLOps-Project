023f3fac1372aaabd28b4152ffa48cf16ac4da32
lxie9 <lxie9@depaul.edu>
2025-05-21 23:14:34 -0700

    Update PHASE2

---
+++ fake_news_detection/profile_train.py
@@ -0,0 +1,12 @@
+import cProfile
+import pstats
+from pstats import SortKey
+from omegaconf import OmegaConf
+from fake_news_detection.train_model import train_with_cfg
+
+if __name__ == "__main__":
+    cfg = OmegaConf.load("../config.yaml")
+    cProfile.runctx("train_with_cfg(cfg)", globals(), locals(), "reports/figures/train_profile.prof")
+
+    stats = pstats.Stats("reports/figures/train_profile.prof")
+    stats.sort_stats(SortKey.CUMULATIVE).print_stats(30)
